79f380c0b8ffd26d66e02d3ed04986738b4c1096
Kover Marcell <1228marci@google.com>
2026-09-15 13:30:26 +0200

    plusz error handling

--- src/electricRat.ts
+++ src/electricRat.ts
@@ -7,6 +7,12 @@ export class ElectricRat {
         if (name == undefined || name == null || name == "") {
             throw new Error("Nem lehet ures a nev");
         }
+        if (atk <= 0) {
+            throw new Error("Az atk nem lehet negativ vagy nulla");
+        }
+        if (hp <= 0) {
+            throw new Error("A hp nem lehet negativ vagy nulla");
+        }
         this.name = name;
         this.atk = atk;
         this.hp = hp;
